9ef9a6f97c54dea497ecb48646d17c555cac2969
ronald2117 <93461532+ronald2117@users.noreply.github.com>
2024-11-26 18:21:40 +0800

    Finish new user data insertion in database functionality in Registration form

--- RestaurantMS/RegistrationForm.Designer.vb
+++ RestaurantMS/RegistrationForm.Designer.vb
@@ -22,9 +22,8 @@ Partial Class RegistrationForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim combobox_account_type As ComboBox
         Panel1 = New Panel()
-        Label5 = New Label()
+        cbtn_submit = New CircularButton()
         txtbox_email = New TextBox()
         Label1 = New Label()
         Label4 = New Label()
@@ -39,17 +38,13 @@ Partial Class RegistrationForm
         txt_Welcome = New Label()
         txtbox_username = New TextBox()
         FlowLayoutPanel1 = New FlowLayoutPanel()
-        cbtn_submit = New CircularButton()
-        combobox_account_type = New ComboBox()
         Panel1.SuspendLayout()
         SuspendLayout()
         ' 
         ' Panel1
         ' 
-        Panel1.BackColor = Color.FromArgb(44, 41, 41)
+        Panel1.BackColor = Color.FromArgb(CByte(44), CByte(41), CByte(41))
         Panel1.Controls.Add(cbtn_submit)
-        Panel1.Controls.Add(combobox_account_type)
-        Panel1.Controls.Add(Label5)
         Panel1.Controls.Add(txtbox_email)
         Panel1.Controls.Add(Label1)
         Panel1.Controls.Add(Label4)
@@ -68,29 +63,37 @@ Partial Class RegistrationForm
         Panel1.Size = New Size(860, 468)
         Panel1.TabIndex = 0
         ' 
-        ' Label5
+        ' cbtn_submit
         ' 
-        Label5.AutoSize = True
-        Label5.BackColor = Color.Transparent
-        Label5.Font = New Font("Microsoft Sans Serif", 12F)
-        Label5.ForeColor = Color.WhiteSmoke
-        Label5.Location = New Point(160, 350)
-        Label5.Name = "Label5"
-        Label5.Size = New Size(106, 20)
-        Label5.TabIndex = 20
-        Label5.Text = "Account Type"
+        cbtn_submit.BackColor = Color.FromArgb(CByte(226), CByte(226), CByte(226))
+        cbtn_submit.BackgroundColor = Color.FromArgb(CByte(226), CByte(226), CByte(226))
+        cbtn_submit.BorderColor = Color.PaleVioletRed
+        cbtn_submit.BorderRadius = 33
+        cbtn_submit.BorderSize = 0
+        cbtn_submit.FlatAppearance.BorderSize = 0
+        cbtn_submit.FlatAppearance.MouseDownBackColor = Color.FromArgb(CByte(94), CByte(90), CByte(90))
+        cbtn_submit.FlatAppearance.MouseOverBackColor = Color.FromArgb(CByte(205), CByte(203), CByte(203))
+        cbtn_submit.FlatStyle = FlatStyle.Flat
+        cbtn_submit.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        cbtn_submit.ForeColor = Color.FromArgb(CByte(25), CByte(24), CByte(24))
+        cbtn_submit.Location = New Point(371, 401)
+        cbtn_submit.Name = "cbtn_submit"
+        cbtn_submit.Size = New Size(149, 43)
+        cbtn_submit.TabIndex = 7
+        cbtn_submit.Text = "Submit"
+        cbtn_submit.TextColor = Color.FromArgb(CByte(25), CByte(24), CByte(24))
+        cbtn_submit.UseVisualStyleBackColor = False
         ' 
         ' txtbox_email
         ' 
-        txtbox_email.BackColor = Color.FromArgb(44, 41, 41)
+        txtbox_email.BackColor = Color.FromArgb(CByte(44), CByte(41), CByte(41))
         txtbox_email.BorderStyle = BorderStyle.FixedSingle
         txtbox_email.Font = New Font("Microsoft Sans Serif", 12F)
         txtbox_email.ForeColor = Color.White
-        txtbox_email.Location = New Point(287, 311)
+        txtbox_email.Location = New Point(288, 330)
         txtbox_email.Name = "txtbox_email"
-        txtbox_email.PasswordChar = "*"c
         txtbox_email.Size = New Size(365, 26)
-        txtbox_email.TabIndex = 19
+        txtbox_email.TabIndex = 5
         ' 
         ' Label1
         ' 
@@ -98,7 +101,7 @@ Partial Class RegistrationForm
         Label1.BackColor = Color.Transparent
         Label1.Font = New Font("Microsoft Sans Serif", 12F)
         Label1.ForeColor = Color.White
-        Label1.Location = New Point(160, 270)
+        Label1.Location = New Point(161, 289)
         Label1.Name = "Label1"
         Label1.Size = New Size(115, 20)
         Label1.TabIndex = 16
@@ -110,7 +113,7 @@ Partial Class RegistrationForm
         Label4.BackColor = Color.Transparent
         Label4.Font = New Font("Microsoft Sans Serif", 12F)
         Label4.ForeColor = Color.White
-        Label4.Location = New Point(160, 313)
+        Label4.Location = New Point(161, 332)
         Label4.Name = "Label4"
         Label4.Size = New Size(48, 20)
         Label4.TabIndex = 17
@@ -118,14 +121,14 @@ Partial Class RegistrationForm
         ' 
         ' txtbox_phone_number
         ' 
-        txtbox_phone_number.BackColor = Color.FromArgb(44, 41, 41)
+        txtbox_phone_number.BackColor = Color.FromArgb(CByte(44), CByte(41), CByte(41))
         txtbox_phone_number.BorderStyle = BorderStyle.FixedSingle
-        txtbox_phone_number.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0)
+        txtbox_phone_number.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         txtbox_phone_number.ForeColor = Color.White
-        txtbox_phone_number.Location = New Point(287, 268)
+        txtbox_phone_number.Location = New Point(288, 287)
         txtbox_phone_number.Name = "txtbox_phone_number"
         txtbox_phone_number.Size = New Size(365, 26)
-        txtbox_phone_number.TabIndex = 18
+        txtbox_phone_number.TabIndex = 4
         ' 
         ' Label3
         ' 
@@ -133,7 +136,7 @@ Partial Class RegistrationForm
         Label3.BackColor = Color.Transparent
         Label3.Font = New Font("Microsoft Sans Serif", 12F)
         Label3.ForeColor = Color.White
-        Label3.Location = New Point(160, 90)
+        Label3.Location = New Point(161, 109)
         Label3.Name = "Label3"
         Label3.Size = New Size(86, 20)
         Label3.TabIndex = 14
@@ -141,26 +144,25 @@ Partial Class RegistrationForm
         ' 
         ' txtbox_first_name
         ' 
-        txtbox_first_name.BackColor = Color.FromArgb(44, 41, 41)
+        txtbox_first_name.BackColor = Color.FromArgb(CByte(44), CByte(41), CByte(41))
         txtbox_first_name.BorderStyle = BorderStyle.FixedSingle
-        txtbox_first_name.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0)
+        txtbox_first_name.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         txtbox_first_name.ForeColor = Color.White
-        txtbox_first_name.Location = New Point(287, 88)
+        txtbox_first_name.Location = New Point(288, 107)
         txtbox_first_name.Name = "txtbox_first_name"
         txtbox_first_name.Size = New Size(365, 26)
-        txtbox_first_name.TabIndex = 15
+        txtbox_first_name.TabIndex = 0
         ' 
         ' txtbox_last_name
         ' 
-        txtbox_last_name.BackColor = Color.FromArgb(44, 41, 41)
+        txtbox_last_name.BackColor = Color.FromArgb(CByte(44), CByte(41), CByte(41))
         txtbox_last_name.BorderStyle = BorderStyle.FixedSingle
         txtbox_last_name.Font = New Font("Microsoft Sans Serif", 12F)
         txtbox_last_name.ForeColor = Color.White
-        txtbox_last_name.Location = New Point(287, 132)
+        txtbox_last_name.Location = New Point(288, 151)
         txtbox_last_name.Name = "txtbox_last_name"
-        txtbox_last_name.PasswordChar = "*"c
         txtbox_last_name.Size = New Size(365, 26)
-        txtbox_last_name.TabIndex = 13
+        txtbox_last_name.TabIndex = 1
         ' 
         ' Label2
         ' 
@@ -168,7 +170,7 @@ Partial Class RegistrationForm
         Label2.BackColor = Color.Transparent
         Label2.Font = New Font("Microsoft Sans Serif", 12F)
         Label2.ForeColor = Color.White
-        Label2.Location = New Point(160, 132)
+        Label2.Location = New Point(161, 151)
         Label2.Name = "Label2"
         Label2.Size = New Size(86, 20)
         Label2.TabIndex = 11
@@ -176,15 +178,15 @@ Partial Class RegistrationForm
         ' 
         ' txtbox_password
         ' 
-        txtbox_password.BackColor = Color.FromArgb(44, 41, 41)
+        txtbox_password.BackColor = Color.FromArgb(CByte(44), CByte(41), CByte(41))
         txtbox_password.BorderStyle = BorderStyle.FixedSingle
         txtbox_password.Font = New Font("Microsoft Sans Serif", 12F)
         txtbox_password.ForeColor = Color.White
-        txtbox_password.Location = New Point(287, 223)
+        txtbox_password.Location = New Point(288, 242)
         txtbox_password.Name = "txtbox_password"
         txtbox_password.PasswordChar = "*"c
         txtbox_password.Size = New Size(365, 26)
-        txtbox_password.TabIndex = 9
+        txtbox_password.TabIndex = 3
         ' 
         ' lbl_username
         ' 
@@ -192,7 +194,7 @@ Partial Class RegistrationForm
         lbl_username.BackColor = Color.Transparent
         lbl_username.Font = New Font("Microsoft Sans Serif", 12F)
         lbl_username.ForeColor = Color.White
-        lbl_username.Location = New Point(160, 182)
+        lbl_username.Location = New Point(161, 201)
         lbl_username.Name = "lbl_username"
         lbl_username.Size = New Size(83, 20)
         lbl_username.TabIndex = 4
@@ -204,7 +206,7 @@ Partial Class RegistrationForm
         lbl_password.BackColor = Color.Transparent
         lbl_password.Font = New Font("Microsoft Sans Serif", 12F)
         lbl_password.ForeColor = Color.White
-        lbl_password.Location = New Point(160, 225)
+        lbl_password.Location = New Point(161, 244)
         lbl_password.Name = "lbl_password"
         lbl_password.Size = New Size(78, 20)
         lbl_password.TabIndex = 5
@@ -214,7 +216,7 @@ Partial Class RegistrationForm
         ' 
         txt_Welcome.AutoSize = True
         txt_Welcome.BackColor = Color.Transparent
-        txt_Welcome.Font = New Font("Microsoft Sans Serif", 28.1999989F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, 0)
+        txt_Welcome.Font = New Font("Microsoft Sans Serif", 28.1999989F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
         txt_Welcome.ForeColor = Color.White
         txt_Welcome.Location = New Point(26, 29)
         txt_Welcome.Name = "txt_Welcome"
@@ -224,14 +226,14 @@ Partial Class RegistrationForm
         ' 
         ' txtbox_username
         ' 
-        txtbox_username.BackColor = Color.FromArgb(44, 41, 41)
+        txtbox_username.BackColor = Color.FromArgb(CByte(44), CByte(41), CByte(41))
         txtbox_username.BorderStyle = BorderStyle.FixedSingle
-        txtbox_username.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0)
+        txtbox_username.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         txtbox_username.ForeColor = Color.White
-        txtbox_username.Location = New Point(287, 180)
+        txtbox_username.Location = New Point(288, 199)
         txtbox_username.Name = "txtbox_username"
         txtbox_username.Size = New Size(365, 26)
-        txtbox_username.TabIndex = 6
+        txtbox_username.TabIndex = 2
         ' 
         ' FlowLayoutPanel1
         ' 
@@ -240,49 +242,17 @@ Partial Class RegistrationForm
         FlowLayoutPanel1.Size = New Size(8, 11)
         FlowLayoutPanel1.TabIndex = 1
         ' 
-        ' combobox_account_type
-        ' 
-        combobox_account_type.BackColor = Color.FromArgb(44, 41, 41)
-        combobox_account_type.FlatStyle = FlatStyle.Flat
-        combobox_account_type.ForeColor = Color.White
-        combobox_account_type.FormattingEnabled = True
-        combobox_account_type.Items.AddRange(New Object() {"admin", "customer"})
-        combobox_account_type.Location = New Point(287, 350)
-        combobox_account_type.Name = "combobox_account_type"
-        combobox_account_type.Size = New Size(365, 23)
-        combobox_account_type.TabIndex = 21
-        ' 
-        ' cbtn_submit
-        ' 
-        cbtn_submit.BackColor = Color.FromArgb(226, 226, 226)
-        cbtn_submit.BackgroundColor = Color.FromArgb(226, 226, 226)
-        cbtn_submit.BorderColor = Color.PaleVioletRed
-        cbtn_submit.BorderRadius = 33
-        cbtn_submit.BorderSize = 0
-        cbtn_submit.FlatAppearance.BorderSize = 0
-        cbtn_submit.FlatAppearance.MouseDownBackColor = Color.FromArgb(94, 90, 90)
-        cbtn_submit.FlatAppearance.MouseOverBackColor = Color.FromArgb(205, 203, 203)
-        cbtn_submit.FlatStyle = FlatStyle.Flat
-        cbtn_submit.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0)
-        cbtn_submit.ForeColor = Color.FromArgb(25, 24, 24)
-        cbtn_submit.Location = New Point(371, 401)
-        cbtn_submit.Name = "cbtn_submit"
-        cbtn_submit.Size = New Size(149, 43)
-        cbtn_submit.TabIndex = 22
-        cbtn_submit.Text = "Submit"
-        cbtn_submit.TextColor = Color.FromArgb(25, 24, 24)
-        cbtn_submit.UseVisualStyleBackColor = False
-        ' 
         ' RegistrationForm
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        BackgroundImage = My.Resources.Resources.login_bg1
+        BackgroundImage = My.Resources.Resources.login_bg
         ClientSize = New Size(1249, 641)
         Controls.Add(FlowLayoutPanel1)
         Controls.Add(Panel1)
         Name = "RegistrationForm"
-        Text = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "User Registration"
         Panel1.ResumeLayout(False)
         Panel1.PerformLayout()
         ResumeLayout(False)
@@ -303,7 +273,6 @@ Partial Class RegistrationForm
     Friend WithEvents txtbox_password As TextBox
     Friend WithEvents Label4 As Label
     Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents Label5 As Label
     Friend WithEvents cbtn_connect As CircularButton
     Friend WithEvents cbtn_submit As CircularButton
     Friend WithEvents txtbox_email As TextBox
--- RestaurantMS/RegistrationForm.vb
+++ RestaurantMS/RegistrationForm.vb
@@ -1,14 +1,38 @@
 ﻿Imports MySql.Data.MySqlClient
+Imports Mysqlx
 
 Public Class RegistrationForm
     Private Sub cbtn_submit_Click(sender As Object, e As EventArgs) Handles cbtn_submit.Click
-        Dim connectionString As String = "Server=localhost;Database=TestDB;Uid=root;Pwd=password;"
+        Dim connectionString As String = "server=sql12.freesqldatabase.com;userid=sql12747542;password='g8UBST1eU4';database=sql12747542"
         Dim conn As New MySqlConnection(connectionString)
-        Dim query As String = "INSERT INTO Users (first_name, last_name, username, password, phone_number, email, account_type) VALUES (@fName, @lName, @username, @password, @phoneNumber, @email, @accountType)"
-        Dim cmd As New MySqlCommand(query, conn)
-        cmd.Parameters.AddWithValue("@fName", txtbox_first_name.Text)
-        cmd.Parameters.AddWithValue("@lName", txtbox_last_name.Text)
-        cmd.Parameters.AddWithValue("@username", txtbox_username.Text)
+        Dim login As New LoginForm
+        Try
+            conn.Open()
+            Dim query As String = "INSERT INTO users (first_name, last_name, username, password, phone_number, email, account_type) VALUES (@fName, @lName, @username, @password, @phoneNumber, @email, @accountType)"
+            Dim cmd As New MySqlCommand(query, conn)
 
+            cmd.Parameters.AddWithValue("@fName", txtbox_first_name.Text)
+            cmd.Parameters.AddWithValue("@lName", txtbox_last_name.Text)
+            cmd.Parameters.AddWithValue("@username", txtbox_username.Text)
+            cmd.Parameters.AddWithValue("@password", txtbox_password.Text)
+            cmd.Parameters.AddWithValue("@phoneNumber", txtbox_phone_number.Text)
+            cmd.Parameters.AddWithValue("@email", txtbox_email.Text)
+            cmd.Parameters.AddWithValue("@accountType", "user")
+
+            Dim result As Integer = cmd.ExecuteNonQuery()
+
+            If result > 0 Then
+                MessageBox.Show("Data inserted successfully!")
+            Else
+                MessageBox.Show("No data inserted.")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            conn.Close()
+            Me.Hide()
+            login.Show()
+        End Try
     End Sub
 End Class
