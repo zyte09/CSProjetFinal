a3094eccd89a91bc1e80f98345923d3bb7d34ae1
CCIT1028007 <CCIT1028007@lab.cict>
2024-11-26 11:23:09 +0700

    create code for inserting datas on database[unfinished]

--- RestaurantMS/RegistrationForm.vb
+++ RestaurantMS/RegistrationForm.vb
@@ -1,3 +1,14 @@
-﻿Public Class RegistrationForm
+﻿Imports MySql.Data.MySqlClient
 
+Public Class RegistrationForm
+    Private Sub cbtn_submit_Click(sender As Object, e As EventArgs) Handles cbtn_submit.Click
+        Dim connectionString As String = "Server=localhost;Database=TestDB;Uid=root;Pwd=password;"
+        Dim conn As New MySqlConnection(connectionString)
+        Dim query As String = "INSERT INTO Users (first_name, last_name, username, password, phone_number, email, account_type) VALUES (@fName, @lName, @username, @password, @phoneNumber, @email, @accountType)"
+        Dim cmd As New MySqlCommand(query, conn)
+        cmd.Parameters.AddWithValue("@fName", txtbox_first_name.Text)
+        cmd.Parameters.AddWithValue("@lName", txtbox_last_name.Text)
+        cmd.Parameters.AddWithValue("@username", txtbox_username.Text)
+
+    End Sub
 End Class
